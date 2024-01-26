5314ce6381ed42ae18942ff659a7f77ccbc1660c
Dawit Mellese <dawitm777@gmail.com>
2024-01-26 08:26:26 +0300

    second commit

---
+++ 7_consecutive_days.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# Specify the path to the external file
+file_path = './data.csv'
+
+# Read data into a DataFrame 
+# And parsing the time-related columns to date-time format by using 'parse_dates' function
+df = pd.read_csv(file_path, parse_dates=['Time', 'Time Out', 'Pay Cycle Start Date', 'Pay Cycle End Date'])
+
+# Calculate the consecutive days worked for each employee
+# ASSUMPTION: Each file number is unique
+df['Consecutive Days'] = df.groupby('File Number')['Time'].diff().dt.days
+
+# Find employees who have worked for 7 consecutive days
+# In 'Consecutive Days' column if the value equals 1 he/she worked for 7 Consecutive days  
+
+result = df[df['Consecutive Days'] == 1].groupby(['Employee Name', 'Position ID']).size()
+
+# Write the result to a separate file called output.txt
+
+with open("./output.txt", 'w') as output_file:
+    output_file.write("=================================================\n")
+    output_file.write("Employees works for 7 Consecutive days:\n")
+    output_file.write("=================================================\n")
+    for row in result.reset_index().values:
+        output_file.write(f"{row[0].replace(",", " ")}, {row[1]} \n")
---
+++ less_10_greater_1.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+# Specify the path to the external CSV file
+csv_file_path = './data.csv'
+
+# Read CSV data into a DataFrame
+df = pd.read_csv(csv_file_path, parse_dates=['Time', 'Time Out', 'Pay Cycle Start Date', 'Pay Cycle End Date'])
+
+# Sort the DataFrame by 'Employee Name' and 'Time' for consecutive shift calculations
+df = df.sort_values(['Employee Name', 'Time'])
+
+# Calculate the time difference between consecutive shifts for each employee
+df['Time Difference'] = df.groupby('File Number')['Time'].diff()
+
+# Filter employees with less than 10 hours and greater than 1 hour of time difference between shifts
+filtered_df = df[(df['Time Difference'] < pd.Timedelta(hours=10)) & (df['Time Difference'] > pd.Timedelta(hours=1))]
+
+# Display the result
+with open('./output.txt', 'a') as output_file:
+    output_file.write("\n====================================================================================\n")
+    output_file.write("Employees with less than 10 hours but greater than 1 hour between shifts:\n")
+    output_file.write("======================================================================================\n")
+
+    # To keep track of unique names
+    unique_names = set()  
+
+    # Iterate in the filetred data to write the output in external file
+    for _, row in filtered_df[['Employee Name', 'Position ID']].iterrows():
+        if row['Employee Name'] not in unique_names:
+            output_file.write(f"{row['Employee Name'].replace(',', ' ')}, {row['Position ID']} \n")
+            unique_names.add(row['Employee Name'])
+
+
